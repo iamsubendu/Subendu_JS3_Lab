405a0fc03a6d63f2edc9c6bd3a1fe65cfc79646d
iamsubendu <66836529+iamsubendu@users.noreply.github.com>
2024-07-17 23:26:21 +0530

    resolving cors error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import WEATHER_API_KEY from './apiKey.js';
 console.log(WEATHER_API_KEY);
 function fetchWeatherData(cityInput) {
   fetch(
-    `https://cors-proxy.htmldriven.com/https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${cityInput}&aqi=no`
+    `https://cors-anywhere.herokuapp.com/http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${cityInput}&aqi=no`
   )
     .then((res) => {
       if (!res.ok) {
